32771124d6b975aac180e8430f4a2944235f09d7
Nicolas Jaco <nicolasjaco@MacBook-Air-de-Nicolas.local>
2024-05-18 10:24:14 -0300

    via-cep

--- routes/web.php
+++ routes/web.php
@@ -7,3 +7,4 @@ Route::get('/', [AddressController::class, 'index'])->name('listar');
 Route::get('/consultar-cep', [AddressController::class, 'add'])->name('consultar_cep');
 Route::get('/buscar-endereco', [AddressController::class, 'get'])->name('buscar_endereco');
 Route::post('/salvar-endereco', [AddressController::class, 'save'])->name('salvar_endereco');
+
